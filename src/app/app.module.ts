0055ceb2b522408d78d8d3c7b87e5942d8da0de7
iagojorge <55568314+iagojorge@users.noreply.github.com>
2021-09-19 04:16:06 -0300

    alterações e adições de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { TabelaComponent } from './tabela/tabela.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
